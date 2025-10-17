5b8f789bc48a20356f6d8dfa85b832983b464899
Abhishek Tiwari <131746064+Abhishektiwari6398@users.noreply.github.com>
2025-10-17 22:39:48 +0530

    Update app.ts

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -7,26 +7,39 @@ import graphRoutes from './routes/graphRoutes';
 import { errorHandler } from './utils/errorHandler';
 import cors from 'cors';
 
-
-
 const app = express();
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 
+const allowedOrigins = [
+  process.env.CORS_ORIGIN!,        
+  'http://localhost:3000'           
+];
+
 app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true, 
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
 }));
 
 
 app.use('/api/users', userRoutes);
 app.use('/api/graph', graphRoutes);
 
+
 app.use(errorHandler);
+
 
 mongoose.connect(process.env.DB_URL!)
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(' MongoDB connection error:', err));
+  .catch(err => console.error('MongoDB connection error:', err));
 
 export default app;
